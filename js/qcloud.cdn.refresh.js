25ad3004f5a0b80cccf7ea8e1fd54efe58dd5780
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-12-21 16:19:57 +0000

    Deploy haifuns/haifuns.github.io to github.com/haifuns/haifuns.github.io.git:gh-pages

---
+++ js/qcloud.cdn.refresh.js
@@ -0,0 +1,35 @@
+// Depends on tencentcloud-sdk-nodejs version 4.0.3 or higher
+const tencentcloud = require("tencentcloud-sdk-nodejs");
+
+const CdnClient = tencentcloud.cdn.v20180606.Client;
+
+const clientConfig = {
+  credential: {
+    secretId: process.env.CDN_ID,
+    secretKey: process.env.CDN_KEY
+  },
+  region: "",
+  profile: {
+    httpProfile: {
+      endpoint: "cdn.tencentcloudapi.com",
+    },
+  },
+};
+
+console.log(clientConfig)
+
+const client = new CdnClient(clientConfig);
+const params = {
+    "Paths": [
+        "https://haifuns.com"
+    ],
+    "FlushType": "flush"
+};
+client.PurgePathCache(params).then(
+  (data) =&gt; {
+    console.log(data);
+  },
+  (err) =&gt; {
+    console.error("error", err);
+  }
+);
